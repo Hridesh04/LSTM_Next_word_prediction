cfdd41802d3880898173089847b342c0ee64aa30
Hridesh04 <hrideshshukla22@gmail.com>
2026-04-13 14:20:35 +0530

    Refactor code for next word prediction app

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 ## Load the trained model 
-from tensorflow.keras.modls import load_model
+from tensorflow.keras.models import load_model
 model = load_model("next_word_lstm.h5")
 ## load the tokenizer
 with open('tokenizer.pickle', 'rb') as handle:
